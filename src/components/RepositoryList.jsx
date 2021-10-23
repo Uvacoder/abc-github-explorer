4132a43a146ce6cbabe1db40793a729d1df70947
Alice Coelho <alicecomoura@outlook.com>
2021-10-23 19:08:39 -0300

    Render API data with map & props

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -4,12 +4,6 @@ import { RepositoryItem } from "./RepositoryItem"
 
 import '../styles/repositories.scss';
 
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/alicecomoura'
-}
-
 export function RepositoryList() {
     const [repositories, setRepositories] = useState([]);
 
@@ -43,10 +37,12 @@ export function RepositoryList() {
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
+                {repositories.map(repository => {
+                    return <RepositoryItem 
+                                key={repository.name} 
+                                repository={repository} 
+                            />
+                })}
             </ul>
         </section>
     )
